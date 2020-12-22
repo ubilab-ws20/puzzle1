32ffa4cfaae1571d151404cddadf4c0e42128c54
Gild3roy <willipaul@web.de>
2020-12-22 18:21:19 +0100

    testing

--- lib/puzzle_3/puzzle3.dart
+++ lib/puzzle_3/puzzle3.dart
@@ -4,6 +4,7 @@ import 'package:ubilab_scavenger_hunt/puzzle_base/puzzleBase.dart';
 import 'package:flutter/services.dart';
 import '../puzzle_base/puzzleBase.dart';
 
+
 class Puzzle3 extends PuzzleBase {
   LatLng getStartLocation() {
     return LatLng(48.012684, 7.835044);
