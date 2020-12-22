f6317556f8e8610ede4c4c209253e5c1be120685
abhisheksharma0217 <74540327+abhisheksharma0217@users.noreply.github.com>
2020-12-22 23:41:38 +0100

    Adding the latest files
    Adding files for puzzle-2 screen_1.

--- lib/puzzle_2/puzzle2.dart
+++ lib/puzzle_2/puzzle2.dart
@@ -2,7 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:latlong/latlong.dart';
 import 'package:ubilab_scavenger_hunt/puzzle_base/puzzleBase.dart';
 
+import 'package:ubilab_scavenger_hunt/puzzle_2/puzzle2Screen.dart';
+import 'package:page_transition/page_transition.dart';
+
 class Puzzle2 extends PuzzleBase {
+
+  static Puzzle2 _instance;
+
+  Puzzle2() {
+    _instance = this;
+  }
+
+  /// Static singleton method.
+  static Puzzle2 getInstance() {
+    if (_instance == null) {
+      _instance = Puzzle2();
+    }
+    return _instance;
+  }
+
   LatLng getStartLocation() {
     return LatLng(48.012684, 7.835044);
   }
@@ -14,6 +32,12 @@ class Puzzle2 extends PuzzleBase {
   }
   void startPuzzle(BuildContext context) {
     print("Puzzle 2 started!");
-    onFinished();
+    // onFinished();
+    Navigator.of(context).push(
+        PageTransition(
+          type: PageTransitionType.fade,
+          child: Puzzle2Screen(),
+        )
+    );
   }
 }
